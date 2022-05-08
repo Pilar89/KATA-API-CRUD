93c414b7ef292bd9539f50011727a6bcadd98838
pilar <ingeniera.coral@gmail.com>
2022-05-08 03:13:57 -0500

    mejoras back datos -- Se crea la base de datos  en el servidor MySQL local

---
+++ front/src/ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="p-3 mb-2 bg-primary text-white">
+        <h1>Lista de tareas</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
